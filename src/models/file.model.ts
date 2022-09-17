5107ad03ad729591685274463d522ecd60c05135
JezielPaladino <jezielpaladino@gmail.com>
2022-09-17 18:34:14 -0300

    CRUD de Archivo y Usuario

---
+++ src/models/file.model.ts
@@ -0,0 +1,37 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class File extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'number',
+  })
+  size?: number;
+
+  @property({
+    type: 'number',
+  })
+  userId?: number;
+
+  constructor(data?: Partial<File>) {
+    super(data);
+  }
+}
+
+export interface FileRelations {
+  // describe navigational properties here
+}
+
+export type FileWithRelations = File & FileRelations;
